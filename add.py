ddf6b0d201a9f5969cd4103cb4ed6e4e4a374374
YGC20 <126743685+YGC20@users.noreply.github.com>
2023-05-16 11:34:44 +0900

    Update add.py

--- add.py
+++ add.py
@@ -1,3 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.sidebar.title("This is written inside the sidebar")
+df = pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.76,-122.4],colums=['lat','lon'])
+st.map(df)
